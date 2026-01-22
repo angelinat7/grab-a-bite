4e502c0391d631114332901eb0f11ed2ddb98450
Angelina Terzieva <angelinaterzieva.s3@learn.telerikacademy.com>
2026-01-22 20:47:21 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import RootNavigator from './navigation/RootNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <StatusBar style='auto' />
-        {/* <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View> */}
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <StatusBar style='auto' />
+
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
