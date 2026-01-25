17af744141c1a6af57aa34c509fed29339a8112f
Angelina Terzieva <angelinaterzieva.s3@learn.telerikacademy.com>
2026-01-25 23:49:08 +0200

    Adds hardcoded data

---
+++ src/data/categoriesData.js
@@ -0,0 +1,8 @@
+export const categories = [
+  { id: 'category-1', title: 'Burgers' },
+  { id: 'category-2', title: 'Pizza' },
+  { id: 'category-3', title: 'Salads' },
+  { id: 'category-4', title: 'Drinks' },
+  { id: 'category-5', title: 'Desserts' },
+  { id: 'category-6', title: 'Sides' },
+];
